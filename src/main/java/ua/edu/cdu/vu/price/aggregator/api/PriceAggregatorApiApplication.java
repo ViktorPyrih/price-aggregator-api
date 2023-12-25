65d75123ef0b3b32ecf4297ff37792cc20621033
ViktorPyrih <v24102002>
2023-12-25 15:59:29 +0200

    Initial commit

---
+++ src/main/java/ua/edu/cdu/vu/price/aggregator/api/PriceAggregatorApiApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.cdu.vu.price.aggregator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceAggregatorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceAggregatorApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/edu/cdu/vu/price/aggregator/api/PriceAggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.cdu.vu.price.aggregator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceAggregatorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
