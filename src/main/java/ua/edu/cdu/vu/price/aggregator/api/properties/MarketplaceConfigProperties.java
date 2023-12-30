baf9bb8c25f5d72bf5472481df7ad960b708311d
ViktorPyrih <v24102002>
2023-12-30 16:20:50 +0200

    Added MarketplaceConfigProperties for marketplace categories

---
+++ src/main/java/ua/edu/cdu/vu/price/aggregator/api/properties/MarketplaceConfigProperties.java
@@ -0,0 +1,39 @@
+package ua.edu.cdu.vu.price.aggregator.api.properties;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import java.util.List;
+import java.util.Map;
+
+@Value
+@Validated
+@ConfigurationProperties(prefix = "price-aggregator-api")
+public class MarketplaceConfigProperties {
+
+    @NotEmpty
+    Map<String, @Valid Part> marketplaceConfig;
+
+    @Value
+    public static class Part {
+        @NotBlank
+        String url;
+        @Valid
+        CategoriesConfig categories;
+        @Valid
+        CategoriesConfig subcategories1;
+        @Valid
+        CategoriesConfig subcategories2;
+    }
+
+    @Value
+    public static class CategoriesConfig {
+        List<String> actions;
+        @NotBlank
+        String selector;
+    }
+}
