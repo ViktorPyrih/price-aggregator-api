24e3bd307c23084b37715551c35bc2da6f4b4256
ViktorPyrih <v24102002>
2024-06-15 13:15:50 +0300

    Extended SelenideConfig with ability to turn screenshots/page-source on/off

--- src/main/java/ua/edu/cdu/vu/price/aggregator/api/config/SelenideConfig.java
+++ src/main/java/ua/edu/cdu/vu/price/aggregator/api/config/SelenideConfig.java
@@ -1,20 +1,29 @@
 package ua.edu.cdu.vu.price.aggregator.api.config;
 
 import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ua.edu.cdu.vu.price.aggregator.api.util.driver.SelenideWebDriver;
 
+@Getter
+@Setter
 @Configuration
+@ConfigurationProperties("price-aggregator-api.selenide")
 public class SelenideConfig {
 
-    @Value("${price-aggregator-api.selenide.timeout:4000}")
     private int timeout;
+    private boolean screenshotsEnabled;
+    private boolean pageSourceEnabled;
 
     @PostConstruct
     public void init() {
         com.codeborne.selenide.Configuration.timeout = timeout;
+        com.codeborne.selenide.Configuration.screenshots = screenshotsEnabled;
+        com.codeborne.selenide.Configuration.savePageSource = pageSourceEnabled;
     }
 
     @Bean
