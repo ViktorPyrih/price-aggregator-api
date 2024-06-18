8ef6d033de7d93c74a078a42d7357ec75edbdd68
ViktorPyrih <v24102002>
2024-06-19 02:40:14 +0300

    Revert "Refactored ProductsResponseMapper so not to filter out products without any part of it"
    This reverts commit 4f0e2fbbc2bcdcd4276eb325e8a1f58d04666d80.

--- src/main/java/ua/edu/cdu/vu/price/aggregator/api/mapper/ProductsResponseMapper.java
+++ src/main/java/ua/edu/cdu/vu/price/aggregator/api/mapper/ProductsResponseMapper.java
@@ -20,18 +20,18 @@ public interface ProductsResponseMapper {
                                                List<String> productTitles,
                                                List<String> productPrices,
                                                int pagesCount) {
-        int productsCount = max(productLinks.size(), productImages.size(), productPriceImages.size(), productDescriptionImages.size(), productTitles.size(), productPrices.size());
+        int productsCount = min(productLinks.size(), productImages.size(), productPriceImages.size(), productDescriptionImages.size(), productTitles.size(), productPrices.size());
 
         return ProductsResponse.builder()
                 .products(IntStream.range(0, productsCount)
                         .mapToObj(i -> ProductsResponse.Product.builder()
                                 .marketplace(marketplace)
-                                .link(get(productLinks, i))
-                                .image(get(productImages, i))
-                                .priceImage(get(productPriceImages, i))
-                                .descriptionImage(get(productDescriptionImages, i))
-                                .title(get(productTitles, i))
-                                .price(get(productPrices, i))
+                                .link(productLinks.get(i))
+                                .image(productImages.get(i))
+                                .priceImage(productPriceImages.get(i))
+                                .descriptionImage(productDescriptionImages.get(i))
+                                .title(productTitles.get(i))
+                                .price(productPrices.get(i))
                                 .build())
                         .toList())
                 .pagesCount(pagesCount)
@@ -48,17 +48,9 @@ public interface ProductsResponseMapper {
         return convertToResponse(marketplace, productLinks, productImages, productPriceImages, productDescriptionImages, productTitles, productPrices, 1);
     }
 
-    private static String get(List<String> list, int index) {
-        if (index >= list.size()) {
-            return null;
-        }
-
-        return list.get(index);
-    }
-
-    private static int max(int... numbers) {
+    private static int min(int... numbers) {
         return Arrays.stream(numbers)
-                .max()
+                .min()
                 .orElseThrow();
     }
 }
