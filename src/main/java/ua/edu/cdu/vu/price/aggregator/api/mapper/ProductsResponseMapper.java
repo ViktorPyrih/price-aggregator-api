5bef59067790a4e04a270ea734df6547caaac445
ViktorPyrih <v24102002>
2024-06-19 02:50:01 +0300

    Added logging of situations when product lists have different lengths

--- src/main/java/ua/edu/cdu/vu/price/aggregator/api/mapper/ProductsResponseMapper.java
+++ src/main/java/ua/edu/cdu/vu/price/aggregator/api/mapper/ProductsResponseMapper.java
@@ -1,5 +1,6 @@
 package ua.edu.cdu.vu.price.aggregator.api.mapper;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
 import ua.edu.cdu.vu.price.aggregator.api.dto.ProductsResponse;
 
@@ -9,18 +10,24 @@ import java.util.stream.IntStream;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
+@Slf4j
 @Mapper(componentModel = SPRING)
-public interface ProductsResponseMapper {
+public abstract class ProductsResponseMapper {
 
-    default ProductsResponse convertToResponse(String marketplace,
-                                               List<String> productLinks,
-                                               List<String> productImages,
-                                               List<String> productPriceImages,
-                                               List<String> productDescriptionImages,
-                                               List<String> productTitles,
-                                               List<String> productPrices,
-                                               int pagesCount) {
+    public ProductsResponse convertToResponse(String marketplace,
+                                              List<String> productLinks,
+                                              List<String> productImages,
+                                              List<String> productPriceImages,
+                                              List<String> productDescriptionImages,
+                                              List<String> productTitles,
+                                              List<String> productPrices,
+                                              int pagesCount) {
         int productsCount = min(productLinks.size(), productImages.size(), productPriceImages.size(), productDescriptionImages.size(), productTitles.size(), productPrices.size());
+        int maxProductsCount = max(productLinks.size(), productImages.size(), productPriceImages.size(), productDescriptionImages.size(), productTitles.size(), productPrices.size());
+
+        if (productsCount != maxProductsCount) {
+            log.error("Product lists have different lengths, product links: {}, min={}, max={}", productLinks, productsCount, maxProductsCount);
+        }
 
         return ProductsResponse.builder()
                 .products(IntStream.range(0, productsCount)
@@ -38,19 +45,25 @@ public interface ProductsResponseMapper {
                 .build();
     }
 
-    default ProductsResponse convertToResponse(String marketplace,
-                                               List<String> productLinks,
-                                               List<String> productImages,
-                                               List<String> productPriceImages,
-                                               List<String> productDescriptionImages,
-                                               List<String> productTitles,
-                                               List<String> productPrices) {
+    public ProductsResponse convertToResponse(String marketplace,
+                                              List<String> productLinks,
+                                              List<String> productImages,
+                                              List<String> productPriceImages,
+                                              List<String> productDescriptionImages,
+                                              List<String> productTitles,
+                                              List<String> productPrices) {
         return convertToResponse(marketplace, productLinks, productImages, productPriceImages, productDescriptionImages, productTitles, productPrices, 1);
     }
 
     private static int min(int... numbers) {
         return Arrays.stream(numbers)
                 .min()
+                .orElseThrow();
+    }
+
+    private static int max(int... numbers) {
+        return Arrays.stream(numbers)
+                .max()
                 .orElseThrow();
     }
 }
